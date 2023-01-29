166dc022a88440765f1b477cabf51bac248a9ccb
Stacey <stacey6105@gmail.com>
2023-01-29 00:36:25 +0000

    onClick function now renders icon in the target div

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,22 +1,44 @@
 function GameGrid({ isPlayer1, setIsPlayer1 }) {
     const handleClick = (e) => {
-        if (isPlayer1) {
+        if (isPlayer1 && e.target.children[0].alt === "") {
+            e.target.children[0].src = require("../imgs/circle.png")
+            e.target.children[0].alt = "nought"
             setIsPlayer1(!isPlayer1)
-        } else if (!isPlayer1) {
+        } else if (!isPlayer1 && e.target.children[0].alt === "") {
+            e.target.children[0].src = require("../imgs/close.png")
+            e.target.children[0].alt = "cross"
             setIsPlayer1(!isPlayer1)
         }
     }
     return (
         <div className="grid-container">
-            <div className="grid-item top-left" onClick={handleClick}></div>
-            <div className="grid-item top-middle" onClick={handleClick}></div>
-            <div className="grid-item top-right" onClick={handleClick}></div>
-            <div className="grid-item middle-left" onClick={handleClick}></div>
-            <div className="grid-item centre" onClick={handleClick}></div>
-            <div className="grid-item middle-right" onClick={handleClick}></div>
-            <div className="grid-item bottom-left" onClick={handleClick}></div>
-            <div className="grid-item bottom-middle" onClick={handleClick}></div>
-            <div className="grid-item bottom-right" onClick={handleClick}></div>
+            <div className="grid-item top-left" onClick={handleClick}>
+                <img src="" alt=""></img>    
+            </div> 
+            <div className="grid-item top-middle" onClick={handleClick}>
+                <img src="" alt=""></img>    
+            </div>
+            <div className="grid-item top-right" onClick={handleClick}>
+                <img src="" alt=""></img> 
+            </div>
+            <div className="grid-item middle-left" onClick={handleClick}>
+            <   img src="" alt=""></img>
+            </div>
+            <div className="grid-item centre" onClick={handleClick}>
+                <img src="" alt=""></img>  
+            </div>
+            <div className="grid-item middle-right" onClick={handleClick}>
+                <img src="" alt=""></img>  
+            </div>
+            <div className="grid-item bottom-left" onClick={handleClick}>
+                <img src="" alt=""></img>  
+            </div>
+            <div className="grid-item bottom-middle" onClick={handleClick}>
+                <img src="" alt=""></img>   
+            </div>
+            <div className="grid-item bottom-right" onClick={handleClick}>
+                <img src="" alt=""></img>     
+            </div>
         </div>
     );
 }
